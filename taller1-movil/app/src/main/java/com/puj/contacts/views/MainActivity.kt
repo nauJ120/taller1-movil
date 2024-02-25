9578e3fd0041c66b63d975a2521346109154a921
Evolmaster <jorgeetorradoforero@gmail.com>
2024-02-25 10:51:21 -0500

    se putea al leer el json aparentemente

--- taller1-movil/app/src/main/java/com/puj/contacts/views/MainActivity.kt
+++ taller1-movil/app/src/main/java/com/puj/contacts/views/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.puj.contacts.views
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.puj.contacts.databinding.ActivityMainBinding
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.puj.contacts.R
+import com.puj.contacts.adapter.CountryAdapter
+import com.puj.contacts.data.Country
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        val countryListView = findViewById<ListView>(R.id.countryListView)
+        val phoneFab :FloatingActionButton = findViewById(R.id.phoneFab)
+        val jsonString = InputStreamReader(resources.openRawResource(R.raw.countries)).readText()
+        val countryListType = object : TypeToken<List<Country>>() {}.type
+        val countries: List<Country> = Gson().fromJson(jsonString, countryListType)
 
-        binding.buttonContacts.setOnClickListener{
-            startActivity(Intent(this, ContactsActivity::class.java))
-        }
-        binding.buttonClients.setOnClickListener{
-            startActivity(Intent(this, ClientsActivity::class.java))
-        }
+        val adapter = CountryAdapter(this, R.layout.country_card, countries)
+        countryListView.adapter = adapter
 
+        phoneFab.setOnClickListener {
+            // Implementar la acción al hacer clic en el botón de teléfono
+            // Puedes agregar código para realizar una llamada o cualquier otra acción deseada.
+        }
     }
 }
